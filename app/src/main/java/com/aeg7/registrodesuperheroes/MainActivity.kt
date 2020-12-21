7050b70356db081144a9e73a540f595a288da276
antjimriq <antoniojimenezriquelme@outlook.es>
2020-12-21 12:15:28 +0100

    Subida portatil 21_12_2020

--- app/src/main/java/com/aeg7/registrodesuperheroes/MainActivity.kt
+++ app/src/main/java/com/aeg7/registrodesuperheroes/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.aeg7.registrodesuperheroes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aeg7.registrodesuperheroes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.saveButton.setOnClickListener{
+            openDetailActivity()
+        }
+    }
+    private fun openDetailActivity(){
+        val intent = Intent(this,DetailActivity::class.java)
+        startActivity(intent)
     }
 }
